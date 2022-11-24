4349003ab84a3a62cea0a65f607727b868caad21
matheusfsilva <matheus.f.silva@kroton.com.br>
2022-11-24 20:02:56 -0300

    Versão final do arquivo ao finalizar o modulo 2 do curso, aplicando conceitos de herença e recursividade de código, além de facilitar manutenção.

--- modelo.py
+++ modelo.py
@@ -1,57 +1,45 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__like = 0
+        self._like = 0
+
+    @property
+    def likes(self):
+        return self._like
+    
+    def dar_likes(self):
+        self._like += 1
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
         
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
 
-    @property
-    def likes(self):
-        return self.__like
-    
-    def dar_likes(self):
-        self.__like += 1
-    
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-
-
-
+     
 
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
-
 atlanta = Serie('atlanta', 2018, 2)
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}')
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
